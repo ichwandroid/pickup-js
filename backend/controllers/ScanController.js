12eff451f4f873eca7eb2cd27a8818f2bd31d1e0
Ichwan <ichwan.laruku@gmail.com>
2023-04-03 23:02:29 +0700

    berjalan normal, tinggal Join database

--- backend/controllers/ScanController.js
+++ backend/controllers/ScanController.js
@@ -24,8 +24,48 @@ export const getScanById = async (req, res) => {
   }
 };
 
-export const createScan = (req, res) => {};
+export const createScan = async (req, res) => {
+  const { id, status } = req.body;
+  try {
+    const scan = await prisma.scan.create({
+      data: {
+        id: id,
+        status: status,
+      },
+    });
+    res.status(201).json(scan);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
 
-export const updateScan = (req, res) => {};
+export const updateScan = async (req, res) => {
+  const { id, status } = req.body;
+  try {
+    const scan = await prisma.scan.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        id: id,
+        status: status,
+      },
+    });
+    res.status(200).json(scan);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
 
-export const deleteScan = (req, res) => {};
+export const deleteScan = async (req, res) => {
+  try {
+    const scan = await prisma.scan.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(scan);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
